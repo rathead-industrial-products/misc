af8f76b8f1785b1bc77ff96a6478210c9ac3394e
ee-quipment.com <support@ee-quipment.com>
2023-08-07 09:30:27 -0700

    update

--- craps/analysis/come_dont_come_baseline.py
+++ craps/analysis/come_dont_come_baseline.py
@@ -52,74 +52,76 @@ def writeCSV(file_name, data_series=[(),(),()], column_headers= ()):
     f.close()
 
 
-def collectBets(throw, table, bank):
-    # right
-    if table.seven_out:
-        bank['bal_rt
-
-
-
-
 #
 # Main
 #
 
-bank = ['bal_rt': 0, 'wrk_rt': 0, 'bal_wr': 0, 'wrk_wr': 0]        # right balance, right working, wrong balance, wrong working
-table = simple_table()
+table = simple_table.table()
 
+roll.seq(); roll.seq(); roll.seq(); roll.seq()
 shooters = [roll.seq() for i in range(N_SHOOTERS)]
 
+right_bet = 1
+wrong_bet = 1
+
+# [[shooter1-roll1 balance, shooter2-roll1 balance, ..], [shooter1-roll2 balance, shooter2-roll2 balance, ..], ..]
+hist_interim_right = []     # array as described above for rolls that are still in progress
+hist_interim_wrong = []
+hist_completed_right = []   # array as described above for rolls that have 7'ed-out
+hist_completed_wrong = []
+stats_interim_right = []
+stats_completed_right = []
+stats_interim_wrong = []
+stats_completed_wrong = []
+
+for i in range(longestSeq(shooters)+1): # initialize arrays
+    hist_interim_right.append([])
+    hist_interim_wrong.append([])
+    hist_completed_right.append([])
+    hist_completed_wrong.append([])
+    stats_interim_right.append([])
+    stats_completed_right.append([])
+    stats_interim_wrong.append([])
+    stats_completed_wrong.append([])
+
 for s in shooters:
+    bank_right = 0
+    bank_wrong = 0
+    n_throws = 0
     for throw in s:
-        table.comeBet(1)
-        working_right += 1
-        table.dontComeBet(1)
-        working_wrong += 1
-        table._action(throw)
-        # right begging
-        if throw in 
+        table.comeBet(right_bet)
+        bank_right -= right_bet
+        table.dontComeBet(wrong_bet)
+        bank_wrong -= wrong_bet
 
+        table.action(throw)
+        n_throws += 1
+      
+        bank_right += table.collectPayoutRight()
+        bank_wrong += table.collectPayoutWrong()
 
-hist_right = []
-hist_wrong = []
-stats_right = []
-stats_wrong = []
-for i in range(longestSeq(shooters)+1): # create an empty histogram of arrays to hold payouts
-    hist_right.append([])
-    hist_wrong.append([])
-    stats_right.append([])
-    stats_wrong.append([])
-for s in shooters:  # create histogram of payouts vs length of roll sequence
-    hist_right[roll.rollLength(s)].append(roll.payoutRight(s))
-    hist_wrong[roll.rollLength(s)].append(roll.payoutWrong(s))
+        if n_throws != len(s):  # not a 7-out, collect interim bank balance data
+            hist_interim_right[n_throws].append(bank_right + table.workingAmountRight())  # working bets are in play and haven't been lost
+            hist_interim_wrong[n_throws].append(bank_wrong + table.workingAmountWrong())
+
+    # shooter has 7'd-out
+    hist_completed_right[n_throws].append(bank_right)
+    hist_completed_wrong[n_throws].append(bank_wrong)
+
 for i in range(longestSeq(shooters)+1): # calculate statistics on multiple results in each histogram bin
-    if hist_right[i]: stats_right[i] = roll.meanMedianModeStdDev(hist_right[i])
-    if hist_wrong[i]: stats_wrong[i] = roll.meanMedianModeStdDev(hist_wrong[i])
+    if hist_interim_right[i]:   stats_interim_right[i]   = roll.meanMedianModeStdDev(hist_interim_right[i])
+    if hist_completed_right[i]: stats_completed_right[i] = roll.meanMedianModeStdDev(hist_completed_right[i])
+    if hist_interim_wrong[i]:   stats_interim_wrong[i]   = roll.meanMedianModeStdDev(hist_interim_wrong[i])
+    if hist_completed_wrong[i]: stats_completed_wrong[i] = roll.meanMedianModeStdDev(hist_completed_wrong[i])
 
-avg_right = [s[0] if len(s) else 0 for s in stats_right]
-avg_wrong = [s[0] if len(s) else 0 for s in stats_wrong]
+avg_interim_right = [s[0] if len(s) else 0 for s in stats_interim_right]
+avg_completed_right = [s[0] if len(s) else 0 for s in stats_completed_right]
+avg_interim_wrong = [s[0] if len(s) else 0 for s in stats_interim_wrong]
+avg_completed_wrong = [s[0] if len(s) else 0 for s in stats_completed_wrong]
 
-writeCSV("come_baseline.csv", (avg_right, avg_wrong), ("come payout vs roll length", "don't come payout vs roll length"))
+print (avg_interim_right)
+print (avg_completed_right)
+print (avg_interim_wrong)
+print (avg_completed_wrong)
 
-"""
-roll_length = []
-payout_right = []
-payout_wrong = []
-pts_made = []
-pts_riding = []
-for shooter in range(SHOOTERS):
-    seq = roll.seq()
-    roll_length.append(roll.rollLength(seq))
-    payout_right.append(roll.payoutRight(seq))
-    payout_wrong.append(roll.payoutWrong(seq))
-    pts_made.append(roll.pointsMade(seq))
-    pts_riding.append(roll.pointsRiding(seq))
-
-    #print (seq, pts_made[-1], pts_riding[-1], payout_right[-1], payout_wrong[-1])
-
-print ("roll length", roll.meanMedianModeStdDev(roll_length))
-print ("payout right", roll.meanMedianModeStdDev(payout_right))
-print ("payout wrong", roll.meanMedianModeStdDev(payout_wrong))
-print ("points made", roll.meanMedianModeStdDev(pts_made))
-print ("points riding", roll.meanMedianModeStdDev(pts_riding))
-"""
+writeCSV("come_baseline.csv", (avg_interim_right, avg_interim_wrong, avg_completed_right, avg_completed_wrong), ("interim come", "interim don't come", "come", "don't come"))
