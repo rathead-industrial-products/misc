ee44eab533f9b1f6a2cb63edb6d297b9f128d46f
Jeff Mitchell <rathead@mindmentum.com>
2023-11-22 12:50:45 -0800

    update

--- ga/come_bet_only_ga.py
+++ ga/come_bet_only_ga.py
@@ -16,13 +16,13 @@
 #
 # 
 
+import multiprocessing as mp
 import random, sys, time
-sys.path.append("/Users/mitchell/Desktop/github-rathead/misc/craps/")
 import simple_table
 
 DICE_SEQUENCES = "../craps/sequence7/sequence7_10K.txt"
 
-POPULATION_SIZE     = 30         # 20 - 40
+POPULATION_SIZE     = 20         # 20 - 40
 CROSSOVER_RATE      = 0.95      # the chance that two chromosomes exchange some of their parts
 MUTATION_RATE       = 0.05      # how many chromosomes are mutated in one generation ( < 0.05 )
 GENERATIONS         = 50
@@ -75,30 +75,46 @@ def mutate(population, mutation_rate):
             #print("mutate", individual, gene)
             individual[gene] = randomGeneValue(gene) # mutate the gene
 
-def fitness(individual):
-    t = simple_table.table()
-    last_throw = 0
-    bank = 0
-    for s in roll_seq:
-        for throw in s:
-            # bet is dictated by genes
-            bet = individual[len(t.workingPoints())]
-            if last_throw in simple_table.CRAPS:
-                bet += individual[GENE_CRAP]
-                bet = max(0, bet)       # can't bet < 0
-            if last_throw == 11:
-                bet += individual[GENE_11]
-                bet = max(0, bet)       # can't bet < 0
-            t.comeBet(bet)
-            bank -= bet
-            t.roll(throw)
-            bank += t.collectPayout()    # table payoff including amount bet
-            last_throw = throw
-        # print (s, bank)
-    return (bank)
+class fitness:
+    def __init__(self, roll_seq):
+        self.roll_seq = roll_seq
+
+    def __call__(self, individual):
+        t = simple_table.table()
+        last_throw = 0
+        bank = 0
+        for s in self.roll_seq:
+            for throw in s:
+                # bet is dictated by genes
+                bet = individual[len(t.workingPointsRight())]
+                if last_throw in simple_table.CRAPS:
+                    bet += individual[GENE_CRAP]
+                    bet = max(0, bet)       # can't bet < 0
+                if last_throw == 11:
+                    bet += individual[GENE_11]
+                    bet = max(0, bet)       # can't bet < 0
+                t.comeBet(bet)
+                bank -= bet
+                t.action(throw)
+                payout = t.collectPayoutRight()    # table payoff including amount bet
+                bank += payout    # table payoff including amount bet
+                # bank += t.collectPayoutRight()    # table payoff including amount bet
+                # print (bet, throw, payout, bank)
+                last_throw = throw
+            # print (s, bank)
+        return (bank)
 
 def rankPopulation(population):
-    rp = sorted(population, key=fitness, reverse=True)
+    with mp.Pool() as p:
+        fit = p.map(fitness(roll_seq), population)      # fit order is same as population order (map preserves order)
+    rp_ranked_by_fit = sorted(zip(fit, population), reverse=True)
+    (fit, rp) = zip(*rp_ranked_by_fit)
+
+    #print ("d", rp_ranked_by_fit)
+    #print ("d", population)
+    #print ("d", rp)
+
+    #rp = sorted(population, key=fitness(roll_seq), reverse=True)
     return (rp)
 
 def getRollSequences():
@@ -112,36 +128,36 @@ def getRollSequences():
 #
 # Main 
 #
+if __name__ == '__main__':
+    start_time = time.time()
+    random.seed(RANDOM_SEED)
+    pop = initPopulation(POPULATION_SIZE, N_GENES)
+    roll_seq = getRollSequences()
 
-start_time = time.time()
-random.seed(RANDOM_SEED)
-pop = initPopulation(POPULATION_SIZE, N_GENES)
-roll_seq = getRollSequences()
+    for g in range(GENERATIONS):
+        pop = rankPopulation(pop)
+        print ("Generation", g)
 
-for g in range(GENERATIONS):
+        if g % 10 == 0:
+            print (pop[0], fitness(roll_seq)(pop[0]))
+            #print()
+
+        pairs = selection(pop)
+        pop = []
+        for parent in pairs:
+            #for p in parent: print (p, fitness(p))
+            #print ()
+
+            children = crossover(parent[0], parent[1], CROSSOVER_RATE)
+            for c in children:
+                pop.append(c)
+                #print (c, fitness(c))
+            #print ()
+
+        mutate(pop, MUTATION_RATE)
+
     pop = rankPopulation(pop)
     print ("Generation", g)
+    for p in pop: print (p, fitness(roll_seq)(p))
 
-    if g % 10 == 0:
-        print (pop[0], fitness(pop[0]))
-        #print()
-
-    pairs = selection(pop)
-    pop = []
-    for parent in pairs:
-        #for p in parent: print (p, fitness(p))
-        #print ()
-
-        children = crossover(parent[0], parent[1], CROSSOVER_RATE)
-        for c in children:
-            pop.append(c)
-            #print (c, fitness(c))
-        #print ()
-
-    mutate(pop, MUTATION_RATE)
-
-pop = rankPopulation(pop)
-print ("Generation", g)
-for p in pop: print (p, fitness(p))
-
-print('Execution time = %0.1f sec ' % (time.time() - start_time))
+    print('Execution time = %0.1f sec ' % (time.time() - start_time))
