bdd3d22f0d14c5816de076706930b4d5921cf441
Jeff Mitchell <rathead@mindmentum.com>
2023-12-16 10:44:16 -0800

    update'

--- craps/modules/roll.py
+++ craps/modules/roll.py
@@ -3,6 +3,11 @@
 # Roll the dice for a craps game.
 # A sequence is defined as a series of rolls ending with a 7.
 # A trial(n) is collection of sequences totaling at least n rolls.
+#
+# For come and don't come, individually, each roll can be determined
+# to be a win, loss, or draw with no additional information other
+# than the roll sequence.
+#
 # Analyze roll sequences.
 #
 
@@ -30,18 +35,39 @@ def seq():
         seq.append(throw())
     return (seq)
 
-def trial(n, flat=False):
+def seqOutcome():
+    # return a series of 3-tuples: (roll, come_outcome, dont_outcome)
+    # where roll is the throw of the dice and *_outcome is +1/0/-1 reflecting whether the
+    # roll, now or in the future, is a win, loss, or draw.
+    BAR = 12        # don't come bar 12
+    rslt = []
+    s = seq()
+    for i in range(len(s)):
+        # determine if roll is a win/lose/draw  outcome = +1 | 0 | -1
+        # don't come is opposite of come outcome unless BAR is rolled, than it's a draw
+        roll = s[i]
+        if roll in (7, 11) or roll in (s[i+1:]): c_outcome = +1 # 7 winner or point made
+        else: c_outcome = -1
+        d_outcome = 0 if roll == BAR else -c_outcome
+        rslt.append((roll, c_outcome, d_outcome))
+    return (rslt)
+
+def trial(n, outcome=False, flat=False):
     # return a seris of sequences totalling at least n rolls:
+    # if outcome == true return a series of seqOutcome(s)
     # if flat == True return a single flat list instead of a list of sequences
     l = 0
-    trial = [seq()]
+    s = seq if not outcome else seqOutcome
+    trial = [s()]
     l += len(trial[-1])
     while l < n:
-        trial.append(seq())
+        trial.append(s())
         l += len(trial[-1])
     if flat:
         trial = [item for sublist in trial for item in sublist]
     return (trial)
+
+    c_win = sum([item[1] for item in list])
 
 
 def meanMedianModeStdDev(data):
@@ -124,6 +150,17 @@ seed()
 # Main  --  Unit Test
 #
 
+#seed(); print (trial(10))
+#seed(); print (trial(10, flat=True))
+
+seed(); l = trial(10000000, True)
+#print (l)
+c_win = sum([item[1] for sublist in l for item in sublist])
+d_win = sum([item[2] for sublist in l for item in sublist])
+print (c_win, d_win)
+#seed(); print (trial(10, True))
+#seed(); print (trial(10, True, True))
+#
 import unittest
 
 class TestSeqEnd(unittest.TestCase):
