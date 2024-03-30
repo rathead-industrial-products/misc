e6798c830d0a715db9b65effc2fea650e9f00c42
Jeff Mitchell <rathead@mindmentum.com>
2024-03-30 09:56:00 -0700

    update

--- options/fetch_option_quotes.py
+++ options/fetch_option_quotes.py
@@ -32,9 +32,11 @@ import os.path
 # suppress FutureWarnings from pandas
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-from yahoo_fin import stock_info, options
+from   yahoo_fin import stock_info, options
+import yfinance as yf
 
 TICKER = 'RMBS'
+ACTIVE = ('TSLA', 'AAPL', 'F')
 DATA_DIR = "/home/mitchell/stock_options/data"
 
 #
@@ -43,6 +45,11 @@ DATA_DIR = "/home/mitchell/stock_options/data"
 # Database records are individual lines in a json file
 # Files are organized by expiration date, one per month
 #
+
+# return a datetime object with the current East Coast time
+def nowET():
+    et = datetime.datetime.now() + datetime.timedelta(hours=3)
+    return (et)
 
 # str in the form 'YYMMDD'
 def datetimeToStr(dt):
@@ -64,6 +71,20 @@ def contractExpiration(contract_name):
     exp = contract_name[:10][4:]
     return (exp)
 
+# return true if the market is open
+def marketOpen():
+    # see if any of very actively traded stocks have traded in the last minute
+    # if one or more have traded, infer that the market is open
+    # stock_info.get_market_status()    # should return (“PRE”), (“OPEN”), (“POST”), or (“CLOSED”) but is broken
+    # use yfinance to get a current quote as a pandas dataframe with pandas timestamps
+    open_f = False
+    for ticker in ACTIVE:
+        last_trade_dt = list(yf.Ticker(ticker).history(period="1d").to_dict()['Volume'].keys())[0].to_pydatetime()
+        last_trade_dt_naive = last_trade_dt.replace(tzinfo=None)    # to compare with naive nowET
+        if nowET() < last_trade_dt_naive + datetime.timedelta(minutes=1):
+            open_f = True
+    return (open_f)
+
 # store a single quote in a file arranged by expiration date
 # files are named 'YYMMDD.json' where 'YYMMDD' is an option expiration Friday
 def store(quote: dict):
@@ -83,13 +104,24 @@ def recall(fname):
     return (quotes)
 
 
+#
+# Main
+#
+
+if not marketOpen():
+    print ("fetch_option_quotes.py - Market Closed")
+    # exit()        ** Until marketOpen is verified **
+else:
+    print ("fetch_option_quotes.py - Market Open")
+
+
 exp_dates = options.get_expiration_dates(TICKER)
 for ex in exp_dates:
     all = options.get_options_chain(TICKER, ex)
     calls = all['calls']
     puts = all['puts']
     underlying = stock_info.get_live_price(TICKER)
-    time = (datetime.datetime.now() - datetime.timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')
+    time = nowET().strftime('%Y-%m-%d %H:%M:%S')
     for _, row in calls.iterrows():
         record = dict(row.to_dict(), **{'type': 'CALL', 'time': time, 'underlying': underlying})
         store(record)
